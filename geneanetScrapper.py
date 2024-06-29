a95c3190a7886e0a79f4ed5125f9bed6b58500ed
davidAlgis <david.algis@tutamail.com>
2024-06-29 18:38:00 +0200

    Add a way to handle duplicates

--- geneanetScrapper.py
+++ geneanetScrapper.py
@@ -77,10 +77,15 @@ class GeneanetScrapper():
         print("Has success to connect to Geneanet.net")
         return True
 
-    def searchFamilyName(self, name):
+    def searchFamilyName(self, name, first_name = ""):
         css_family_name = 'div.row:nth-child(5) > div:nth-child(1) > input:nth-child(1)'
         family_name_field = self.driver.find_element(css_family_name)
         family_name_field.send_keys(name)
+        
+        if(first_name != ""):
+            css_first_name = "div.row:nth-child(5) > div:nth-child(2) > input:nth-child(1)"
+            first_name_field = self.driver.find_element(css_first_name)
+            first_name_field.send_keys(first_name)
 
         css_search_button = "button.button"
         search_button = self.driver.find_element(css_search_button)
@@ -100,26 +105,114 @@ class GeneanetScrapper():
             break
             self.clickOnNextPage()
 
+        self.merge_individus()
+        
+        self.process_individus()
+
+    def process_individus(self):
         for individu in self.individus:
             last_name = individu.get_last_name()
             first_name = individu.get_first_name()
             date_naissance = individu.get_birth_date()
+
             try:
                 date_naissance = utils.parse_date(date_naissance)
             except ValueError:
                 date_naissance = None
 
-            folder_individu = utils.to_upper_camel_case(
-                last_name + ' ' + first_name)
-            if (date_naissance is None):
-                folder_individu_path = os.path.join(
-                    self.individu_folder, folder_individu)
-            else:
+            folder_individu = utils.to_upper_camel_case(last_name + ' ' + first_name)
+            if date_naissance is not None:
                 folder_individu += f" - {date_naissance.year}"
-                folder_individu_path = os.path.join(
-                    self.individu_folder, folder_individu)
-            GeneanetItemToMd(individu, folder_individu_path,
-                             self.file_path_to_template_individu)
+
+            folder_individu_path = os.path.join(self.individu_folder, folder_individu)
+
+            # Check if folder exists, and if so, add a suffix
+            i = 1
+            original_folder_individu_path = folder_individu_path
+            while os.path.exists(folder_individu_path):
+                folder_individu_path = f"{original_folder_individu_path} - {i}"
+                i += 1
+
+            GeneanetItemToMd(individu, folder_individu_path, self.file_path_to_template_individu)
+
+
+    def merge_individus(self):
+        merged_individus = []
+        visited = set()
+
+        for i, individu in enumerate(self.individus):
+            if i in visited:
+                continue
+
+            # Check for duplicates
+            duplicates = [individu]
+
+            for j, other_individu in enumerate(self.individus[i + 1:], start=i + 1):
+                if j in visited:
+                    continue
+
+                if (individu.last_name == other_individu.last_name and
+                        individu.first_name == other_individu.first_name):
+                    if (individu.birth_date and other_individu.birth_date and
+                            individu.birth_date == other_individu.birth_date) or \
+                            (individu.death_date and other_individu.death_date and
+                             individu.death_date == other_individu.death_date) or \
+                            (individu.wedding_date and other_individu.wedding_date and
+                             individu.wedding_date == other_individu.wedding_date):
+                        duplicates.append(other_individu)
+                        visited.add(j)
+
+            # Merge duplicates
+            merged_individu = self.merge_duplicates(duplicates)
+            merged_individus.append(merged_individu)
+            visited.add(i)
+
+        self.individus = merged_individus
+
+    def merge_duplicates(self, duplicates):
+        merged = duplicates[0]
+        for individu in duplicates[1:]:
+            if not merged.sexe and individu.sexe:
+                merged.set_sex(individu.sexe)
+            if not merged.profession and individu.profession:
+                merged.set_profession(individu.profession)
+            if not merged.civil_state_notes and individu.civil_state_notes:
+                merged.set_civil_state_notes(individu.civil_state_notes)
+            if not merged.civil_state_src and individu.civil_state_src:
+                merged.set_civil_state_src(individu.civil_state_src)
+            if not merged.birth_date and individu.birth_date:
+                merged.set_birth_date(individu.birth_date)
+            if not merged.birth_place and individu.birth_place:
+                merged.set_birth_place(individu.birth_place)
+            if not merged.father and individu.father:
+                merged.set_father(individu.father)
+            if not merged.mother and individu.mother:
+                merged.set_mother(individu.mother)
+            if not merged.birth_notes and individu.birth_notes:
+                merged.set_birth_notes(individu.birth_notes)
+            if not merged.birth_src and individu.birth_src:
+                merged.set_birth_src(individu.birth_src)
+            if not merged.death_date and individu.death_date:
+                merged.set_death_date(individu.death_date)
+            if not merged.death_place and individu.death_place:
+                merged.set_death_place(individu.death_place)
+            if not merged.death_notes and individu.death_notes:
+                merged.set_death_notes(individu.death_notes)
+            if not merged.death_src and individu.death_src:
+                merged.set_death_src(individu.death_src)
+            if not merged.partner and individu.partner:
+                merged.set_partner(individu.partner)
+            if not merged.wedding_date and individu.wedding_date:
+                merged.set_wedding_date(individu.wedding_date)
+            if not merged.wedding_place and individu.wedding_place:
+                merged.set_wedding_place(individu.wedding_place)
+            if not merged.wedding_notes and individu.wedding_notes:
+                merged.set_wedding_notes(individu.wedding_notes)
+            if not merged.wedding_src and individu.wedding_src:
+                merged.set_wedding_src(individu.wedding_src)
+            merged.other_informations.extend(individu.other_informations)
+
+        return merged
 
     def handle_item(self, j):
         css_line_j = f"a.ligne-resultat:nth-child({j})"
